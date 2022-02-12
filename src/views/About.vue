dad422e5fedfb84b7bf9d3ce8937831ccc5e94c8
KornwitPookkamnuan <99543004+KornwitPookkamnuan@users.noreply.github.com>
2022-02-12 20:51:37 +0700

    Add files via upload

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <br>
+    <center>
+    <!--  <h1>This is an about page.</h1>  -->
+    <h1>Kornwit</h1>
+    <h3>6304101303</h3>
+    </center>
+    
+  </div>
+</template>
